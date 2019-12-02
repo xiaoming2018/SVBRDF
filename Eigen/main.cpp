4bec6320b35acd5d77eccb2e445d2fc3b44add6b
xiaoming <1209819655@qq.com>
2019-12-02 22:06:37 +0800

    111

--- Eigen/main.cpp
+++ Eigen/main.cpp
@@ -29,7 +29,7 @@ int main()
 
 	// 矩阵输出
 	cout << matrix_23 << endl;
-	
+
 	// 用（）取值
 	for (size_t i = 0; i < 1; i++)
 	{
@@ -39,11 +39,49 @@ int main()
 		}
 	}
 	v_3d << 3, 2, 1;
-	Eigen::Matrix<double, 2, 1> result = matrix_23.cast<double>() * v_3d;
 
+	// 显示类型转换
+	Eigen::Matrix<double, 2, 1> result = matrix_23.cast<double>() * v_3d;
 	cout << result << endl;
 
+	// 矩阵运算
+	cout << "矩阵运算" << endl;
+	matrix_33 = Eigen::Matrix3d::Random();
+	cout << matrix_33 << endl;
 
+	cout << matrix_33.transpose() << endl; // 转置
+	cout << matrix_33.sum() << endl;       // 个元素和
+	cout << matrix_33.trace() << endl;     // 迹
+	cout << 10 * matrix_33 << endl;        // 数乘
+	cout << matrix_33.inverse() << endl;   // 逆
+	cout << matrix_33.determinant() << endl;  // 行列式
+
+	cout << "特征值：" << endl;
+	// 特征值
+	// 实对称矩阵可以保证对角化成功
+	Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> eigen_solver(matrix_33.transpose() * matrix_33);
+
+	cout << "Eigen Value : " << eigen_solver.eigenvalues() << endl;
+	cout << "Eigen Vector : " << eigen_solver.eigenvectors() << endl;
+
+	// 解方程
+	// 求解 matrix_NN * x = v_Nd 方程
+	Eigen::Matrix<double, MATRIX_SIZE, MATRIX_SIZE> matrix_NN;
+	matrix_NN = Eigen::MatrixXd::Random(MATRIX_SIZE, MATRIX_SIZE);
+	Eigen::Matrix<double, MATRIX_SIZE, 1> v_Nd;
+	v_Nd = Eigen::MatrixXd::Random(MATRIX_SIZE, 1);
+
+	clock_t time_stt = clock(); // 计时
+	// 直接求逆
+	Eigen::Matrix<double, MATRIX_SIZE, 1> x = matrix_NN.inverse() * v_Nd;
+	cout << "time use in normal inverse is "
+		 << 1000 * (clock() - time_stt) / (double)CLOCKS_PER_SEC << "ms" << endl;
+	
+	// 矩阵分解求解，例如 QR分解，
+	time_stt = clock();
+	x = matrix_NN.colPivHouseholderQr().solve(v_Nd);
+	cout << "time use in Qr composition is "
+		<< 1000 * (clock() - time_stt) / (double)CLOCKS_PER_SEC << "ms" << endl;
 	system("pause");
 	return 0;
 }
---
+++ OpenCV_SFM/PolyocularReconstruction.cpp
@@ -0,0 +1,269 @@
+#include <iostream>
+#include <vector>
+#include <io.h>
+#include <fstream>
+#include <string>
+
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
+
+using namespace std;
+using namespace cv;
+
+// 初始化点云结构
+void init_structure(Mat k,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<vector<Vec3b>>& colors_for_all,
+	vector<vector<DMatch>>& matches_for_all,
+	vector<Point3f>& structure,
+	vector<vector<int>>& correspond_structure_idx,
+	vector<Vec3b>& colors,
+	vector<Mat>& rotations,
+	vector<Mat>& motions
+);
+
+void get_matched_points(
+	vector<KeyPoint>& p1,
+	vector<KeyPoint>& p2,
+	vector<DMatch> matches,
+	vector<Point2f>& out_p1,
+	vector<Point2f>& out_p2
+);
+
+void get_matched_colors(
+	vector<Vec3b>& c1,
+	vector<Vec3b>& c2,
+	vector<DMatch> matches,
+	vector<Vec3b>& out_c1,
+	vector<Vec3b>& out_c2
+);
+
+bool find_transform(Mat& k, vector<Point2f>& p1, vector<Point2f>& p2, Mat& R, Mat& T, Mat& mask);
+void maskout_points(vector<Point2f>& p1, Mat& mask);
+void maskout_colors(vector<Vec3b>& p1, Mat& mask);
+void reconstruct(Mat& K, Mat& R1, Mat& T1, Mat& R2, Mat& T2, vector<Point2f>& p1, vector<Point2f>& p2, vector<Point3f>& structure);
+void get_objpoints_and_imgpoints(
+	vector<DMatch>& matches,
+	vector<int>& struct_indices,
+	vector<Point3f>& structure,
+	vector<KeyPoint>& key_points,
+	vector<Point3f>& object_points,
+	vector<Point2f>& image_points
+);
+void fusion_structure(
+	vector<DMatch>& matches,
+	vector<int>& struct_indices,
+	vector<int>& next_struct_indices,
+	vector<Point3f>& structure,
+	vector<Point3f>& next_structure,
+	vector<Vec3b>& colors,
+	vector<Vec3b>& next_colors
+);
+
+// 提取图像特征点
+void extract_features(
+	vector<string>& image_names,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<Mat>& descriptor_for_all,
+	vector<vector<Vec3b>>& colors_for_all
+);
+
+void match_features(Mat& query, Mat& train, vector<DMatch>& matches);
+void match_features(vector<Mat>& descriptor_for_all, vector<vector<DMatch>>& matches_for_all);
+void save_structure(string filename, vector<Mat>& rotations, vector<Mat>& motions, vector<Point3f>& structure, vector<Vec3b>& colors);
+
+std::string dir = "/images/";
+
+int main()
+{
+	vector<string> img_names;
+	img_names.push_back(dir + "0000.jpg");
+
+	// 内参矩阵
+	Mat K(Matx33d(
+		2759.48, 0, 1520.69,
+		0, 2764.16, 1006.81,
+		0, 0, 1));
+	
+	// 特征点
+	vector<vector<KeyPoint>> key_points_for_all;
+	// 描述子
+	vector<Mat> descriptor_for_all;
+	vector<vector<Vec3b>> colors_for_all;
+	vector<vector<DMatch>> matches_for_all;
+
+	// 提取所有图像的特征
+	extract_features(img_names, key_points_for_all, descriptor_for_all, colors_for_all);
+	// 对所有图像进行顺次的特征匹配
+	match_features(descriptor_for_all, matches_for_all);
+
+	vector<Point3f> structure;
+	// 保存第i副图像中第j特征点对应的structure中的索引
+	vector<vector<int>> correspond_struct_idx; 
+	vector<Vec3b> colors;
+	vector<Mat> rotations;
+	vector<Mat> motions;
+
+	// 初始化结构（三维点云）第一和第二幅
+	init_structure(K,
+		key_points_for_all,
+		colors_for_all,
+		matches_for_all,
+		structure,
+		correspond_struct_idx,
+		colors,
+		rotations,
+		motions);
+
+
+	system("pause");
+	return 0;
+}
+
+void extract_features(
+	vector<string>& image_names,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<Mat>& descriptor_for_all,
+	vector<vector<Vec3b>>& colors_for_all
+	)
+{
+	key_points_for_all.clear();
+	descriptor_for_all.clear();
+	Mat image;
+
+	// 读取图像，获取图像特征点并保存
+	Ptr<Feature2D> sift = xfeatures2d::SIFT::create(0, 3, 0.04, 10);
+	for (auto it = image_names.begin(); it != image_names.end(); it++)
+	{
+		image = imread(*it);
+		if (image.empty())
+			continue;
+		cout << "Extracing freatures : " << *it << endl;
+
+		vector<KeyPoint> key_points;
+		Mat descriptor;
+		sift->detect(image, key_points);
+		sift->compute(image, key_points, descriptor);
+
+		// 特征点过少，则排除该图像
+		if (key_points.size() <= 10)
+			continue;
+
+		key_points_for_all.push_back(key_points);
+		descriptor_for_all.push_back(descriptor);
+
+		// 三通道 存放三通道颜色
+		vector<Vec3b> colors(key_points.size());
+		for (int i = 0; i < key_points.size(); i++)
+		{
+			Point2f& p = key_points[i].pt;
+			if (p.x <= image.rows && p.y <= image.cols)
+				colors[i] = image.at<Vec3b>(p.x, p.y);
+		}
+		colors_for_all.push_back(colors);
+	}
+}
+
+void match_features(vector<Mat>& descriptor_for_all, vector<vector<DMatch>>& matches_for_all)
+{
+	matches_for_all.clear();
+	// n 个图像，两两顺次有 n-1 对匹配
+	for (int i = 0; i < descriptor_for_all.size()-1; i++)
+	{
+		cout << "Matching images " << i << " - " << i + 1 << endl;
+		vector<DMatch> matches;
+		match_features(descriptor_for_all[i], descriptor_for_all[i + 1], matches);
+		matches_for_all.push_back(matches);
+	}
+}
+
+void match_features(Mat& query, Mat& train, vector<DMatch>& matches)
+{
+	vector<vector<DMatch>> knn_matches;
+	BFMatcher matcher(NORM_L2);
+	matcher.knnMatch(query, train, knn_matches, 2);
+
+	// 获取满足 Radio Test 的最小匹配的距离
+	float min_dist = FLT_MAX;
+	for (int r = 0; r < knn_matches.size(); r++)
+	{
+		// Radio Test
+		if (knn_matches[r][0].distance > 0.6 * knn_matches[r][1].distance)
+			continue;
+		float dist = knn_matches[r][0].distance;
+		if (dist < min_dist)
+		{
+			min_dist = dist;
+		}
+	}
+
+	matches.clear();
+	for (size_t i = 0; i < knn_matches.size(); i++)
+	{
+		if (knn_matches[i][0].distance > 0.6 * knn_matches[i][1].distance ||
+			knn_matches[i][0].distance > 5 * max(min_dist, 10.0f))
+			continue;
+		// 保存匹配点
+		matches.push_back(knn_matches[i][0]);
+	}
+}
+
+void init_structure(
+	Mat K,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<vector<Vec3b>>& colors_for_all,
+	vector<vector<DMatch>>& matches_for_all,
+	vector<Point3f>& structure,
+	vector<vector<int>>& correspond_struct_idx,
+	vector<Vec3b>& colors,
+	vector<Mat>& rotations,
+	vector<Mat>& motions
+)
+{
+	// 计算头两幅图像之间的变换矩阵
+	vector<Point2f> p1, p2;
+	vector<Vec3b> c2;
+	Mat R, T; // 旋转，平移
+	Mat mask; // mask中大于零的点代表匹配点，等于零的点代表失配点
+	get_matched_points(key_points_for_all[0], key_points_for_all[1], matches_for_all[0], p1, p2);
+	get_matched_colors(colors_for_all[0], colors_for_all[1], matches_for_all[0], colors, c2);
+	find_transform(K, p1, p2, R, T, mask); // 三角分解R T 矩阵
+
+	// 对头两幅图像进行三维重建
+	maskout_points(p1, mask);
+	maskout_points(p2, mask);
+	maskout_colors(colors, mask);
+
+	
+
+	Mat R0 = Mat::eye(3, 3, CV_64FC1);
+	Mat T0 = Mat::zeros(3, 1, CV_64FC1);
+	reconstruct(K, R0, T0, R, T, p1, p2, structure);
+
+	// 保存变换矩阵
+	rotations = { R0, R };
+	motions = { T0, T };
+
+	// 将correspond_struct_idx的大小初始化为与key_points_for_all完全一致
+	correspond_struct_idx.clear();
+	correspond_struct_idx.resize(key_points_for_all.size());
+	for (size_t i = 0; i < key_points_for_all.size(); i++)
+	{
+		correspond_struct_idx[i].resize(key_points_for_all[i].size());
+	}
+
+	// 填写头两副图像的结构索引
+	int idx = 0;
+	vector<DMatch>& matches = matches_for_all[0];
+	for (size_t i = 0; i < matches.size(); i++)
+	{
+		if (mask.at<uchar>(i) == 0)
+			continue;
+		correspond_struct_idx[0][matches[i].queryIdx] = idx;
+		correspond_struct_idx[1][matches[i].trainIdx] = idx;
+		++idx;
+	}
+}
